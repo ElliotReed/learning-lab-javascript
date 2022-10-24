c18be3223fb088d89035bdd39ae82ab0c0019637
elliotreed <elliot@elliotreed.net>
2022-10-24 11:41:21 -0600

    added starter and solutionfiles

---
+++ arrays/app.starter..js
@@ -0,0 +1,20 @@
+// ? Copy app.starter.js to app.js
+
+// * Using forLoop(), add a value to every value in an array of numbers and return it.
+function forLoop(array, value) { }
+
+// * Using forLoopBreak(), add a value to every value < 100 in an array of numbers and return it.
+function forLoopBreak(array, value) { }
+
+// * Using whileLoop(), create a program that returns an array of items with the first 3 items replaced with '*'.
+function whileLoop(array) { }
+
+// * Using doWhileLoop(), create a program that returns an array of items with the first 3 items replaced with '*'.
+function doWhileLoop(array) { }
+
+module.exports = {
+  forLoop,
+  forLoopBreak,
+  whileLoop,
+  doWhileLoop,
+}
